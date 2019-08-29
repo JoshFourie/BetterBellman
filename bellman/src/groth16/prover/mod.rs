94f1799643423add2b6a73f1722b596c7665e05e
Josh Fourie <josh.fourie97@gmail.com>
2019-08-29 23:51:13 +1000

    Refactor prover
    Signed-off-by: Josh Fourie <josh.fourie97@gmail.com>

--- bellman/src/groth16/prover/mod.rs
+++ bellman/src/groth16/prover/mod.rs
@@ -48,7 +48,7 @@ where
     let vk = params.get_vk(prover.assignment.input.len())?;
 
     let h = {
-        let a: _ = prover.primefield(&worker)?;
+        let a: _ = prover.fft_shortcut(&worker)?;
         multiexp(&worker, params.get_h(a.len())?, FullDensity, a)
     };
 
@@ -84,90 +84,64 @@ where
         a_aux_density_total
     )?;
 
-    let a_inputs = multiexp(
-        &worker,
-        a_inputs_source,
-        FullDensity,
-        input_assignment.clone(),
-    );
-    let a_aux = multiexp(
-        &worker,
-        a_aux_source,
-        Arc::new(prover.density.a_aux),
-        aux_assignment.clone(),
-    );
-
-    let b_input = Arc::new(prover.density.b_input);
-    let b_input_total = b_input.get_total_density();
+    let b_input_density = Arc::new(prover.density.b_input);
     let b_aux_density = Arc::new(prover.density.b_aux);
+
+    let b_input_total = b_input_density.get_total_density();
     let b_aux_density_total = b_aux_density.get_total_density();
 
-    let (b_g1_inputs_source, b_g1_aux_source) =
-        params.get_b_g1(b_input_total, b_aux_density_total)?;
+    let (b1_input_src, b1_aux_src): _ = ProvingSystem::src_gb1(
+        b_input_density.clone(),
+        b_aux_density.clone(), 
+        &mut params
+    )?;
 
-    let b_g1_inputs = multiexp(
-        &worker,
-        b_g1_inputs_source,
-        b_input.clone(),
-        input_assignment.clone(),
-    );
-    let b_g1_aux = multiexp(
-        &worker,
-        b_g1_aux_source,
-        b_aux_density.clone(),
-        aux_assignment.clone(),
-    );
-
-    let (b_g2_inputs_source, b_g2_aux_source) =
-        params.get_b_g2(b_input_total, b_aux_density_total)?;
-
-    let b_g2_inputs = multiexp(
-        &worker,
-        b_g2_inputs_source,
-        b_input,
-        input_assignment,
-    );
-    let b_g2_aux = multiexp(&worker, b_g2_aux_source, b_aux_density, aux_assignment);
+    let (b2_input_src, b2_aux_src): _ = ProvingSystem::src_gb2(
+        b_input_total,
+        b_aux_density_total,
+        &mut params
+    )?;
 
     if vk.delta_g1.is_zero() || vk.delta_g2.is_zero() {
         // If this element is zero, someone is trying to perform a
         // subversion-CRS attack.
         return Err(SynthesisError::UnexpectedIdentity);
     }
+    
+    let answer: _ = AnswerContext::try_new::<P>(
+        &worker, 
+        a_inputs_source,
+        b1_input_src,
+        b2_input_src,
+        b_input_density,
+        input_assignment
+    )?;
 
-    let mut g_a = vk.delta_g1.mul(r);
-    g_a.add_assign_mixed(&vk.alpha_g1);
-    let mut g_b = vk.delta_g2.mul(s);
-    g_b.add_assign_mixed(&vk.beta_g2);
-    let mut g_c;
-    {
-        let mut rs = r;
-        rs.mul_assign(&s);
+    let aux: _ = AuxiliaryContext::try_new::<P>(
+        &worker,
+        a_aux_source,
+        b1_aux_src,
+        b2_aux_src,
+        prover.density.a_aux,
+        b_aux_density,
+        aux_assignment
+    )?;
 
-        g_c = vk.delta_g1.mul(rs);
-        g_c.add_assign(&vk.alpha_g1.mul(s));
-        g_c.add_assign(&vk.beta_g1.mul(r));
-    }
-    let mut a_answer = a_inputs.wait()?;
-    a_answer.add_assign(&a_aux.wait()?);
-    g_a.add_assign(&a_answer);
-    a_answer.mul_assign(s);
-    g_c.add_assign(&a_answer);
+    let builder: _ = GroupBuilder {
+        vk: &vk,
+        r,
+        s,
+        answer,
+        aux,
+        h: h.wait()?,
+        l: l.wait()?
+    };
 
-    let mut b1_answer = b_g1_inputs.wait()?;
-    b1_answer.add_assign(&b_g1_aux.wait()?);
-    let mut b2_answer = b_g2_inputs.wait()?;
-    b2_answer.add_assign(&b_g2_aux.wait()?);
-
-    g_b.add_assign(&b2_answer);
-    b1_answer.mul_assign(r);
-    g_c.add_assign(&b1_answer);
-    g_c.add_assign(&h.wait()?);
-    g_c.add_assign(&l.wait()?);
+    let (ga,gb,gc): _ = builder.try_build()?;
 
     Ok(Proof {
-        a: g_a.into_affine(),
-        b: g_b.into_affine(),
-        c: g_c.into_affine(),
+        a: ga.into_affine(),
+        b: gb.into_affine(),
+        c: gc.into_affine(),
     })
 }
--- bellman/src/groth16/prover/system.rs
+++ bellman/src/groth16/prover/system.rs
@@ -1,8 +1,9 @@
 use rand_core::RngCore;
 
 use std::sync::Arc;
+use super::{Future, SynthesisError};
 
-use ff::{Field, PrimeField};
+use ff::{Field, PrimeField, ScalarEngine};
 use pairing::Engine;
 
 use super::{ParameterSource, Proof, Result};
@@ -12,7 +13,7 @@ use crate::domain::{EvaluationDomain, Scalar};
 use crate::multiexp::{multiexp, DensityTracker, FullDensity};
 use crate::multicore::Worker;
 use crate::groth16::VerifyingKey;
-use group::CurveAffine;
+use group::{CurveAffine, CurveProjective};
 
 pub struct ProvingSystem<E: Engine> {
     pub density: QueryDensity,
@@ -159,8 +160,8 @@ impl<E: Engine> ProvingSystem<E> {
         super::create_proof::<E, C, P>(circuit, params, r, s)
     }
 
-    pub fn primefield(&mut self, worker: &Worker) ->  Result<Arc<Vec<<<E as ff::ScalarEngine>::Fr as PrimeField>::Repr>>> {
-        let field: _ = ProvingField::new(&mut self.eval, worker)?;
+    pub fn fft_shortcut(&mut self, worker: &Worker) ->  Result<Arc<Vec<<<E as ff::ScalarEngine>::Fr as PrimeField>::Repr>>> {
+        let field: _ = FourierField::new(&mut self.eval, worker)?;
         let mut a: _ = field.transform().into_coefficient();
 
         let new_len = a.len() - 1;
@@ -170,43 +171,34 @@ impl<E: Engine> ProvingSystem<E> {
             .map(|s| s.0.into_repr())
             .collect();
 
+
         Ok(Arc::new(repr))
     }   
 
-    pub fn into_variables<P>(self, worker: &Worker, mut params: P) -> Result<()> 
+    pub fn src_gb1<T>(
+        input_density: Arc<DensityTracker>, 
+        aux_density: Arc<DensityTracker>, 
+        params: &mut T
+    ) -> Result<(T::G1Builder, T::G1Builder)> 
     where
-        P: ParameterSource<E>
+        T: ParameterSource<E>
     {
-        let aux: Arc<Vec<_>> = Arc::new(
-            self.assignment
-                .aux
-                .into_iter()
-                .map(|s| s.into_repr())
-                .collect()
-        );
-        let input: Arc<_> = Arc::new(
-            self.assignment
-                .input
-                .into_iter()
-                .map(|s| s.into_repr())
-                .collect::<Vec<_>>()
-        );
-        let l = multiexp(
-            &worker,
-            params.get_l(aux.len())?,
-            FullDensity,
-            aux.clone(),
-        );
+        let b_input_total = input_density.get_total_density();
+        let b_aux_density_total = aux_density.get_total_density();
 
-        unimplemented!()
+        Ok(params.get_b_g1(b_input_total, b_aux_density_total)?)
     }
 
-    fn build_ga(vk: VerifyingKey<E>, r: E::Fr) -> () {
-        let deltar: _ = vk.delta_g1.mul(r);
-
+    pub fn src_gb2<U>(
+        input_density: usize, 
+        aux_density: usize, 
+        params: &mut U
+    ) -> Result<(U::G2Builder, U::G2Builder)> 
+    where
+        U: ParameterSource<E>
+    {
+        Ok(params.get_b_g2(input_density, aux_density)?)
     }
-
-    
 }
 
 impl<E: Engine> Default for ProvingSystem<E> {
@@ -216,6 +208,171 @@ impl<E: Engine> Default for ProvingSystem<E> {
             eval: PolynomialEvaluation::default(),
             assignment: ProvingAssignment::default()
         }
+    }
+}
+
+pub struct GroupBuilder<'a, E: Engine> {
+    pub vk: &'a VerifyingKey<E>, 
+    pub r: E::Fr, 
+    pub s: E::Fr, 
+    pub answer: AnswerContext<E>,
+    pub aux: AuxiliaryContext<E>,
+    pub h: E::G1,
+    pub l: E::G1
+}
+
+impl<'a,E> GroupBuilder<'a,E>
+where
+    E: Engine
+{
+    pub fn try_new<P>(prover: ProvingSystem<E>, params: P) -> Result<Self> 
+    where
+        P: ParameterSource<E>
+    {
+        unimplemented!()
+    }
+
+    pub fn try_build(mut self) -> Result<(E::G1, E::G2, E::G1)> {
+        let ga: _ = self.try_ga()?;
+        let gb: _ = self.try_gb()?;
+        let gc: _ = self.try_gc()?;
+        Ok((ga, gb, gc))
+    }
+
+    fn try_ga(&mut self) -> Result<E::G1> {
+        let mut ga: _ = self.vk.delta_g1.mul(self.r);
+        ga.add_assign_mixed(&self.vk.alpha_g1);
+
+        self.answer.a.add_assign(&self.aux.a);
+        ga.add_assign(&self.answer.a);
+        
+        Ok(ga)
+    }
+
+    fn try_gb(&mut self) -> Result<E::G2> {
+        let mut gb: _ = self.vk.delta_g2.mul(self.s);
+        gb.add_assign_mixed(&self.vk.beta_g2);
+
+        self.answer.b2.add_assign(&self.aux.b2);
+        gb.add_assign(&self.answer.b2);
+
+        Ok(gb)
+    }   
+
+    fn try_gc(mut self) -> Result<E::G1> {
+        let delta_rs: E::G1 = {
+            let mut rs: _ = self.r; 
+            rs.mul_assign(&self.s);
+            self.vk.delta_g1.mul(rs)
+        };
+        let As: _ = self.vk.alpha_g1.mul(self.s);
+        let Br: _ = self.vk.beta_g1.mul(self.r);
+
+        let mut gc: _ = delta_rs;
+        gc.add_assign(&As);
+        gc.add_assign(&Br);
+
+        self.answer.a.mul_assign(self.s);
+        gc.add_assign(&self.answer.a);
+
+        self.answer.b1.add_assign(&self.aux.b1);
+        self.answer.b1.mul_assign(self.r);
+        gc.add_assign(&self.answer.b1);
+
+        gc.add_assign(&self.h);
+        gc.add_assign(&self.l); 
+
+        Ok(gc)
+    } 
+}
+
+pub struct AnswerContext<E: Engine> {
+    a: E::G1,
+    b1: E::G1,
+    b2: E::G2
+}
+
+impl<E> AnswerContext<E>
+where
+    E: Engine
+{
+    pub fn try_new<P: ParameterSource<E>>(
+        worker: &Worker, 
+        a_src: P::G1Builder, 
+        b1_src: P::G1Builder, 
+        b2_src: P::G2Builder,
+        b_in: Arc<DensityTracker>,
+        input: Arc<Vec<<<E as ff::ScalarEngine>::Fr as PrimeField>::Repr>>
+    ) -> Result<Self> {
+        let a: E::G1 = multiexp(
+            &worker,
+            a_src,
+            FullDensity,
+            input.clone(),
+        ).wait()?;
+
+        let b1: E::G1 = multiexp(
+            &worker,
+            b1_src,
+            b_in.clone(),
+            input.clone(),
+        ).wait()?;
+
+        let b2: E::G2 = multiexp(
+            &worker,
+            b2_src,
+            b_in,
+            input
+        ).wait()?;
+
+        Ok(AnswerContext { a, b1, b2 })
+    }
+}
+
+pub struct AuxiliaryContext<E: Engine> {
+    a: E::G1,
+    b1: E::G1,
+    b2: E::G2,  
+}
+
+impl<E> AuxiliaryContext<E> 
+where
+    E: Engine
+{
+    pub fn try_new<P>(
+        worker: &Worker,
+        a_src: P::G1Builder,
+        b1_src: P::G1Builder,
+        b2_src: P::G2Builder,
+        a_density: DensityTracker,
+        b_density: Arc<DensityTracker>,
+        assignment: Arc<Vec<<<E as ScalarEngine>::Fr as PrimeField>::Repr>>
+    ) -> Result<Self> 
+    where
+        P: ParameterSource<E>
+    {
+        let a: _ = multiexp(
+            &worker,
+            a_src,
+            Arc::new(a_density),
+            assignment.clone(),
+        ).wait()?;
+
+        let b1: _ = multiexp(
+            &worker,
+            b1_src,
+            b_density.clone(),
+            assignment.clone(),
+        ).wait()?;
+
+        let b2 = multiexp(
+            &worker, 
+            b2_src, 
+            b_density, 
+            assignment
+        ).wait()?;
+
+        Ok(AuxiliaryContext{ a, b1, b2 })
     }
 }
 
@@ -265,14 +422,14 @@ impl<E: Engine> Default for ProvingAssignment<E> {
     }
 } 
 
-struct ProvingField<'a, E: Engine> {
+struct FourierField<'a, E: Engine> {
     a: EvaluationDomain<E,Scalar<E>>,
     b: EvaluationDomain<E,Scalar<E>>,
     c: EvaluationDomain<E,Scalar<E>>,
     work: &'a Worker
 }
 
-impl<'a,E> ProvingField<'a,E> 
+impl<'a,E> FourierField<'a,E> 
 where
     E: Engine
 {
@@ -280,7 +437,7 @@ where
         let a = EvaluationDomain::from_coeffs(eval.a.take()?)?;
         let b = EvaluationDomain::from_coeffs(eval.b.take()?)?;
         let c = EvaluationDomain::from_coeffs(eval.c.take()?)?;
-        Ok(ProvingField {a, b, c, work})
+        Ok(FourierField {a, b, c, work})
     }
 
     fn transform(mut self) -> Self {
